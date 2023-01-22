2ac91a71535818a932fc4362a627755b7a3298c8
Minhaz Khan <mk16che011bsmrstu@gmail.com>
2023-01-22 14:14:55 +0600

    nl5

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader, PostCard } from '../../components';
 import { getPosts, getPostDetails } from '../../services';
 import { AdjacentPosts } from '../../sections';
 
@@ -17,6 +17,9 @@ const PostDetails = ({ post }) => {
       <div className="container mx-auto px-10 mb-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="col-span-1 lg:col-span-8">
+          {post.map((postn, index) => (
+            <PostCard key={index} post={postn.node} />
+          ))}
             <PostDetail post={post} />
             <Author author={post.author} />
             <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
