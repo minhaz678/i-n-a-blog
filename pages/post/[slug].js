7c3f70a3116957366c1e5108317a45b6407759a4
Minhaz Khan <mk16che011bsmrstu@gmail.com>
2023-01-22 13:48:41 +0600

    nl

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -48,10 +48,10 @@ export async function getStaticProps({ params }) {
 
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
-// export async function getStaticPaths() {
-//   const posts = await getPosts();
-//   return {
-//     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-//     fallback: true,
-//   };
-// }
+export async function getStaticPaths() {
+  const posts = await getPosts();
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: true,
+  };
+}
