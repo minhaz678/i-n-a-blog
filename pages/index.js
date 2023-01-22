cde38884c7607ee85d317fd91017ff9a571fdd33
Minhaz Khan <mk16che011bsmrstu@gmail.com>
2023-01-22 16:34:23 +0600

    nl556

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,59 @@
 import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
-import { getPosts } from '../services';
+// import { getPosts } from '../services';
+import { request, gql } from 'graphql-request';
+import { useRouter } from 'next/router'
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.postsConnection.edges;
+};
 
 export default function Home({ posts }) {
+  const router = useRouter()
+
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
+  // Render post...
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <FeaturedPosts />
@@ -23,6 +74,16 @@ export default function Home({ posts }) {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    // Only `/posts/1` and `/posts/2` are generated at build time
+    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+    // Enable statically generating additional pages
+    // For example: `/posts/3`
+    fallback: true,
+  }
+}
+
 // Fetch data at build time
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
@@ -31,14 +92,5 @@ export async function getStaticProps() {
   };
 }
 
-// Specify dynamic routes to pre-render pages based on data.
-// The HTML is generated at build time and will be reused on each request.
-export async function getStaticPaths() {
-  const posts = await getPosts();
-  return {
-    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: true,
-  };
-}
 
 
